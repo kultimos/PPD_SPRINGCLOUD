41242e57bb725722b535c437178ae52593eeafc2
wushuai <659-4gpe145ln@dingtalk.com>
2023-04-21 17:25:00 +0800

    p7:done

--- eureka/eureka-client-02-a/src/main/java/com/kul/EurekaClientApplication.java
+++ eureka/eureka-client-02-a/src/main/java/com/kul/EurekaClientApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient //表明自己是一个客户端
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
--- eureka/eureka-server-01/src/main/java/com/kul/EurekaServerApplication.java
+++ eureka/eureka-server-01/src/main/java/com/kul/EurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //server端需要添加该注解表明,client端会使用@EnableEurekaClient注解
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
