1f11d6054a84c526d0cb926453419e2bc4fee47d
wushuai <659-4gpe145ln@dingtalk.com>
2023-04-25 15:57:51 +0800

    p15:restTemplate示例&使用表单参数发送post请求的controller接收写法和post请求体的构建方式

---
+++ eureka/rest-template/src/main/java/com/kul/domain/User.java
@@ -0,0 +1,16 @@
+package com.kul.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    String name;
+
+    Integer age;
+
+    Double price;
+}
