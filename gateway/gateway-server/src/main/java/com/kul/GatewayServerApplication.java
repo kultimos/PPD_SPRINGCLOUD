5b5f12d8ac33cf47715783f744223079d6b7ee1b
wushuai <659-4gpe145ln@dingtalk.com>
2023-05-25 20:17:27 +0800

    p48:通过将gateway服务注册到eureka上,进而通过注册中心应用名方式访问服务实现动态路由,例:http://localhost:80/login-service/login

--- gateway/gateway-server/src/main/java/com/kul/GatewayServerApplication.java
+++ gateway/gateway-server/src/main/java/com/kul/GatewayServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
