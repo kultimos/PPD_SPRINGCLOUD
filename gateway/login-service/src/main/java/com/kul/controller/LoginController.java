1ee2434a152307a3b3986928819ebf845ebda1d1
wushuai <659-4gpe145ln@dingtalk.com>
2023-05-29 10:18:28 +0800

    p49:断言工厂简单的小李子

--- gateway/login-service/src/main/java/com/kul/controller/LoginController.java
+++ gateway/login-service/src/main/java/com/kul/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.kul.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
 @RestController
 public class LoginController {
 
@@ -10,6 +13,11 @@ public class LoginController {
     public String doLogin(String name,String pwd){
         System.out.println("name = " + name);
         System.out.println("pwd = " + pwd);
-        return "success";
+        return UUID.randomUUID().toString();
+    }
+
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
     }
 }
