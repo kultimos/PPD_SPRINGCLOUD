e4a9957590ce36e7105ccc3711b7019e71216bc4
wushuai <659-4gpe145ln@dingtalk.com>
2023-06-05 17:07:22 +0800

    p63:nacos的隔离性,只有同一个命名空间下的同一个组中的服务可以相互发现

---
+++ nacos/01-nacos-client-a/src/main/java/com/kul/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.kul.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/test")
+    public String test() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("nacos-client-b");
+        System.out.println(instances);
+        return "test";
+    }
+}
